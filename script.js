be0c7a193ae5a7edb2f89e88c5a2c85d9784c605
Md Rayhan ul Fardous <49313477+rayhan-fardous@users.noreply.github.com>
2025-06-16 17:20:56 +0600

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const form = document.getElementById("form");
 const password1El = document.getElementById("password1");
 const password2El = document.getElementById("password2");
-const messageContainer = document.querySelector("message-container");
+const messageContainer = document.querySelector(".message-container");
 const message = document.getElementById("message");
 
 let isValid = false;
@@ -39,10 +39,26 @@ function validateForm() {
   }
 }
 
+function storeFormData() {
+  const user = {
+    name: form.name.value,
+    phone: form.phone.value,
+    email: form.email.value,
+    website: form.website.value,
+    password: form.password.value
+  };
+  // Do something with user data
+  console.log(user);
+}
+
 function processFormData(e) {
   e.preventDefault();
   // Validate Form
   validateForm();
+  // Submit Data if Valid
+  if (isValid && passwordsMatch) {
+    storeFormData();
+  }
 }
 
 // Event Listener
