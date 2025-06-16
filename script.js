b517108f5bddefdff3a964bb5f9c5b78cdcecbb0
Md Rayhan ul Fardous <49313477+rayhan-fardous@users.noreply.github.com>
2025-06-16 12:34:05 +0600

    validating password

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
-const form = document.getElementById('form');
-const password1El = document.getElementById('password1');
-const password2El = document.getElementById('password2');
-const messageContainer = document.querySelector('message-container');
-const message = document.getElementById('message');
+const form = document.getElementById("form");
+const password1El = document.getElementById("password1");
+const password2El = document.getElementById("password2");
+const messageContainer = document.querySelector("message-container");
+const message = document.getElementById("message");
 
 let isValid = false;
+let passwordsMatch = false;
 
 function validateForm() {
   // Using Contraint API
   isValid = form.checkValidity();
-  // Style main message for an error 
-  message.textContent = 'Please fill out all fields.';
-  message.style.color = 'red';
-  messageContainer.style.borderColor = 'red';
+  // Style main message for an error
+  if (!isValid) {
+    message.textContent = "Please fill out all fields.";
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red";
+    return;
+  }
+  //Check to see if passwords match
+  if (password1El.value === password2El.value) {
+    passwordsMatch = true;
+    password1El.style.borderColor = "green";
+    password2El.style.borderColor = "green";
+  } else {
+    passwordsMatch = false;
+    message.textContent = "Make sure passwords match";
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red";
+    password1El.style.borderColor = "red";
+    password2El.style.borderColor = "red";
+    return;
+  }
+  // If form is valid and password match
+  if (isValid && passwordsMatch) {
+    message.textContent = "Successfully Registered!";
+    message.style.color = "green";
+    messageContainer.style.borderColor = "green";
+  }
 }
 
 function processFormData(e) {
@@ -22,4 +46,4 @@ function processFormData(e) {
 }
 
 // Event Listener
-form.addEventListener('submit', processFormData)
+form.addEventListener("submit", processFormData);
